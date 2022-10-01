0c73e1f0e4de7e2e4957480a7dc2d4429b1590e3
Vágó Rebeka <rebeka.vago19@gmail.com>
2022-10-01 23:07:50 +0200

    adatok elküldése esemény kész

--- felvetel.php
+++ felvetel.php
@@ -20,14 +20,92 @@
     <main class="container">
         <h1>Kutyák felvétele</h1>
         
+        <?php if (isset($_REQUEST['submit_btn'])) : ?>
+            <?php if (count($_POST) < 6) : ?>
+                    <?php echo '<div class="alert alert-danger" role="alert">Hiba!'; ?>
+                    <?php if (!isset($_POST['nev']) || empty($_POST['nev'])) : ?>
+                    <?php echo '<br>Név megadása kötelező!'; ?>
+                    <?php endif; ?>
+                    <?php if (!isset($_POST['fajta']) || empty($_POST['fajta'])) : ?>
+                    <?php echo '<br>Fajta megadása kötelező!'; ?>
+                    <?php endif; ?>
+                    <?php if (!isset($_POST['kor']) || empty($_POST['kor'])) : ?>
+                    <?php echo '<br>Kor megadása kötelező!'; ?>
+                    <?php endif; ?>
+                    <?php if (!isset($_POST['nem']) || empty($_POST['nem'])) : ?>
+                    <?php echo '<br>Nem megadása kötelező!'; ?>
+                    <?php endif; ?>
+                    <?php if (!isset($_POST['szin']) || empty($_POST['szin'])) : ?>
+                    <?php echo '<br>Szín megadása kötelező!'; ?>
+                    <?php echo '</div>'; ?>
+                    <?php endif; ?>
+                </ul>
+            <?php else : ?>
+                <?php 
+                    $file = fopen("adatok.csv", 'a');
+                    $nev = $_POST['nev'];
+                    $fajta = "";
+                    switch ($_POST['fajta']){
+                        case "golden":
+                            $fajta = "Golden retriever";
+                            break;
+                        case "labrador":
+                            $fajta = "Labrador";
+                            break;
+                        case "spaniel":
+                            $fajta = "Spániel";
+                            break;
+                        case "tibeti":
+                            $fajta = "Tibeti terrier";
+                            break;
+                        case "csivava":
+                            $fajta = "Csivava";
+                            break;
+                    }
+                    $kor = $_POST['kor'];
+                    $nem = "";
+                    switch ($_POST['nem']) {
+                        case "him":
+                            $nem = "Hím";
+                            break;
+                        case "nosteny":
+                            $nem = "Nőstény";
+                            break;
+                    } 
+                    $szin = "";
+                    switch ($_POST['szin']) {
+                        case "fekete":
+                            $szin = "Fekete";
+                            break;
+                        case "feher":
+                            $szin = "Fehér";
+                            break;
+                        case "barna":
+                            $szin = "Barna";
+                            break;
+                        case "szurke":
+                            $szin = "Szürke";
+                            break;
+                        case "tobbszinu":
+                            $szin = "Többszínű";
+                            break;
+                    }
+                    $sor = "$nev;$fajta;$kor;$nem;$szin". PHP_EOL;
+                    fwrite($file, $sor);
+                    fclose($file);
+                    echo '<div class="alert alert-success" role="alert">Sikeres felvétel!</div>'
+                ?>
+            <?php endif; ?>
+        <?php endif; ?>
+
         <form method="POST" name="felvetel" id="felvetel">
             <div class="mb-3">
                 <label class="form-label" for="nev_input">Név:</label>
-                <input class="form-control" type="text" name="nev" id="nev_input" placeholder="Név" required>
+                <input class="form-control" type="text" name="nev" id="nev_input" placeholder="Név">
             </div>
             <div class="mb-3">
                 <label class="form-label" for="fajta_input">Fajta:</label>
-                <select class="form-select" name="fajta" id="fajta_input" required>
+                <select class="form-select" name="fajta" id="fajta_input">
                     <option value=""></option>
                     <option value="golden">Golden retriever</option>
                     <option value="labrador">Labrador</option>
@@ -38,7 +116,7 @@
             </div>
             <div class="mb-3">
                 <label class="form-label" for="kor_input">Kor:</label>
-                <input class="form-control" type="number" name="kor" id="kor_input" required>
+                <input class="form-control" type="number" name="kor" id="kor_input">
             </div>
             <div class="mb-3">
                 <label class="form-label radio_label checkbox_label">Nem:</label>
@@ -51,7 +129,7 @@
             </div>
             <div class="mb-3">
                 <label class="form-label" for="szin_input">Szín:</label>
-                <select class="form-select" name="szin" id="szin_input" required>
+                <select class="form-select" name="szin" id="szin_input">
                     <option value=""></option>
                     <option value="fekete">Fekete</option>
                     <option value="feher">Fehér</option>
@@ -60,11 +138,8 @@
                     <option value="tobbszinu">Többszínű</option>
                 </select>
             </div>
-            <button type="reset" class="btn btn-outline-danger">Űrlap alaphelyzetbe</button>
-            <button type="submit" class="btn btn-outline-primary">Elküld</button>
+            <button type="submit" class="btn btn-outline-primary" name="submit_btn">Elküld</button>
         </form>
-    
     </main>
-    
 </body>
 </html>
