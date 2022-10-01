e834d226ac8ddfca01f9b7eb11b4523617460a32
Vágó Rebeka <rebeka.vago19@gmail.com>
2022-10-01 20:11:25 +0200

    bootstrap, linkek hozzáadva

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kutyák</title>
+    
+    <!-- Latest compiled and minified CSS -->
+    <link href="bootstrap.min.css" rel="stylesheet">
+
+    <!-- Latest compiled JavaScript -->
+    <script src="bootstrap.bundle.min.js"></script>
+
+</head>
+<body>
+    <nav class="navbar navbar-expand navbar-primary bg-light">
+        <ul class="navbar-nav">
+            <li class="nav-item"><a class="nav-link" href="index.php">Listázás</a></li>
+            <li class="nav-item"><a class="nav-link" href="felvetel.php">Felvétel</a></li>
+        </ul>
+    </nav>
+    
+</body>
+</html>
